ca52b7fb0e30ad01ca4dedafcf0f1c9bbe891dd6
ameyer-eng <72287053+ameyer-eng@users.noreply.github.com>
2022-12-19 12:04:20 -0600

    initial copy from example

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
+var createError = require('http-errors');
 const express = require('express');
-const app = express();
+const hbs = require('hbs');
 const path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-app.listen(3000, ()=>{
-    console.log("app listening on port 3000");
-})
+var indexRouter = require('./routes/index');
+const fibonacci= require('./routes/fibonacci');
 
-app.get('/chemical', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'index.html'))
-})
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+hbs.registerPartials(path.join(__dirname, 'partials'));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/fibonacci', fibonacci);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
